8fc2460aacd097e26da1b5fe99236dd898524602
lukehowkins <luke.howkins@gmail.com>
2016-04-02 09:03:58 +0100

    split styling into seperate less files

--- app/components/ladder-table/component.js
+++ app/components/ladder-table/component.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'table',
+  classNames: 'ladder',
   people: null,
   displayedPeople: Ember.computed('people', function() {
     return this.get('people').map(function(person, i) {
