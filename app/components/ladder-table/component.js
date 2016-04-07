d4ba66f4d2297d2dea6464cb0e63d00b4ba0b70c
howkinl <luke.howkins@ig.com>
2016-04-07 08:42:43 +0100

    added games and frames played

--- app/components/ladder-table/component.js
+++ app/components/ladder-table/component.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   people: null,
   displayedPeople: Ember.computed('people.[]', function() {
     return this.get('people').map((person, i) => {
+      // Extend?
       let tmp = Ember.Object.create({
         position: i + 1,
         name: person.get('name'),
@@ -13,16 +14,23 @@ export default Ember.Component.extend({
         games: person.get('games'),
         challenging: person.get('challenging')
       });
+      let gameWins = person.get('games.wins'),
+          gameLosses = person.get('games.losses'),
+          frameWins = person.get('frames.wins'),
+          frameLosses = person.get('frames.losses');
+
+      tmp.set('games.played', gameWins + gameLosses);
+      tmp.set('frames.played', frameWins + frameLosses);
 
       tmp.set('challenging', person.get('challenging') === null ? null : (person.get('challenging') + 1));
 
       let gameRatio = {
-        winLoss: this._winLossRatio(person.get('games.wins'), person.get('games.losses')),
-        winPercent: this._winPercent(person.get('games.wins'), person.get('games.losses'))
+        winLoss: this._winLossRatio(gameWins, gameLosses),
+        winPercent: this._winPercent(gameWins, gameLosses)
       };
       let framesRatio = {
-        winLoss: this._winLossRatio(person.get('frames.wins'), person.get('frames.losses')),
-        winPercent: this._winPercent(person.get('frames.wins'), person.get('frames.losses'))
+        winLoss: this._winLossRatio(frameWins, frameLosses),
+        winPercent: this._winPercent(frameWins, frameLosses)
       };
 
       tmp.set('games.ratio', gameRatio);
