3156a41ef485fb88418c162901c0d884ab001282
Gagondeep Srai <gagondeep.srai@gmail.com>
2016-04-10 23:49:53 +0100

    add challenges

--- app/models/person.js
+++ app/models/person.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  challenging: DS.attr('number'),
+  challenging: DS.attr('string'),
   games: DS.attr(),
   frames: DS.attr()
 });
