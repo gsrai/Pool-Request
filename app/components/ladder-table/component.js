563beb686c98bcdc28850950b0545c6160ff81c6
Gagondeep Srai <gagondeep.srai@gmail.com>
2016-04-17 18:08:03 +0100

    added position to model, games added to the history now effect the ladder.

--- app/components/ladder-table/component.js
+++ app/components/ladder-table/component.js
@@ -4,10 +4,9 @@ export default Ember.Component.extend({
   classNames: 'ladder',
   people: null,
   displayedPeople: Ember.computed('people.[]', function() {
-    return this.get('people').map((person, i) => {
-      // Extend?
+    let people = this.get('people').map((person, i) => {
       let tmp = Ember.Object.create({
-        position: i + 1,
+        position: person.get('position'),
         name: person.get('name'),
         frames: person.get('frames'),
         games: person.get('games'),
@@ -20,8 +19,6 @@ export default Ember.Component.extend({
 
       tmp.set('games.played', gameWins + gameLosses);
       tmp.set('frames.played', frameWins + frameLosses);
-
-      tmp.set('challenging', person.get('challenging') === null ? null : (person.get('challenging') + 1));
 
       let gameRatio = {
         winLoss: this._winLossRatio(gameWins, gameLosses),
@@ -41,6 +38,9 @@ export default Ember.Component.extend({
 
       return tmp;
     });
+    return people.sort((a, b) => {
+      return a.get('position') - b.get('position');
+    });
   }),
 
   _winLossRatio(wins, losses) {
@@ -48,6 +48,10 @@ export default Ember.Component.extend({
   },
 
   _winPercent(wins, losses) {
-    return ((wins / (wins + losses)).toFixed(2) * 100) + '%';
+    if (wins === 0 && losses === 0) {
+      return '0%';
+    } else {
+      return ((wins / (wins + losses)).toFixed(2) * 100) + '%';
+    }
   }
 });
--- app/controllers/game.js
+++ app/controllers/game.js
@@ -9,19 +9,53 @@ export default Ember.Controller.extend({
   date: moment().format('YYYY-MM-DD'),
   actions: {
     submit() {
-      this.store.createRecord('game', {
-        'name-one': this.get('nameOne'),
-        'score-one': this.get('scoreOne'),
-        'name-two': this.get('nameTwo'),
-        'score-two': this.get('scoreTwo'),
-        'date': new Date(this.get('date'))
-      }).save().then(() => {
-        this.set('nameOne', '');
-        this.set('nameTwo', '');
-        this.transitionToRoute('history');
-      }).catch(function(error) {
-        // TODO maybe should show there was an error on screen
-        console.log('caught: ' + error);
+      let p1 = {name: this.get('nameOne'), score: Number(this.get('scoreOne'))};
+      let p2 = {name: this.get('nameTwo'), score: Number(this.get('scoreTwo'))};
+      this.store.findAll('person').then(function(players) {
+        // assuming nameOne is the winner
+        let winner = players.findBy('name', p1.name);
+        let loser = players.findBy('name', p2.name);
+
+        let wp = winner.get('position');
+        let lp = loser.get('position');
+        // if the winner is the challenger then swap positions
+        winner.set('position', (wp > lp) ? lp : wp);
+        winner.set('games', {
+          wins: winner.get('games.wins') + 1,
+          losses: winner.get('games.losses')
+        });
+        winner.set('frames', {
+          wins: winner.get('frames.wins') + p1.score,
+          losses: winner.get('frames.losses') + p2.score
+        });
+        winner.set('challenging', null);
+
+        loser.set('position', (wp > lp) ? wp : lp);
+        loser.set('games', {
+          wins: loser.get('games.wins'),
+          losses: loser.get('games.losses') + 1
+        });
+        loser.set('frames', {
+          wins: loser.get('frames.wins') + p2.score,
+          losses: loser.get('frames.losses') + p1.score
+        });
+        loser.set('challenging', null);
+        players.save();
+      }).then(() => {
+        this.store.createRecord('game', {
+          'name-one': this.get('nameOne'),
+          'score-one': this.get('scoreOne'),
+          'name-two': this.get('nameTwo'),
+          'score-two': this.get('scoreTwo'),
+          'date': new Date(this.get('date'))
+        }).save().then(() => {
+          this.set('nameOne', '');
+          this.set('nameTwo', '');
+          this.transitionToRoute('history');
+        }).catch(function(error) {
+          // TODO maybe should show there was an error on screen
+          console.log('caught: ' + error);
+        });
       });
     }
   }
