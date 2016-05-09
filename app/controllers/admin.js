c91992c4562eaf832a9d023dc1e9ef5e35f72c29
Luke Howkins <luke.howkins@ig.com>
2016-05-09 19:16:28 +0100

    multi header tidy up

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,16 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   section: 'admin-challenging',
-  'admin-challengingClass': 'active',
-  'admin-playerClass': 'unactive',
-  'admin-gameClass': 'unactive',
-
   actions: {
-    setSection(section) {
-      const oldSection = this.get('section');
-      this.set(`${oldSection}Class`, 'unactive');
-      this.set(`${section}Class`, 'active');
-      this.set('section', section);
+    setSection() {
+      this.send('setSection');
     },
     transitionToGame() {
       this.transitionToRoute('ladder');
