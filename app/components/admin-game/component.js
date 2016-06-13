027e5f9a474cdffa3996d63b1df2a6deae18eb0c
Luke Howkins <luke.howkins@ig.com>
2016-06-13 20:13:00 +0100

    fix scores not updating when adding game

--- app/components/admin-game/component.js
+++ app/components/admin-game/component.js
@@ -10,9 +10,9 @@ export default Ember.Component.extend({
   date: moment().format('YYYY-MM-DD'),
 
   players: Ember.computed('model.[]', 'model.@each.challenging', function() {
-    let players = this.get('model');
+    const players = this.get('model');
     return players.map((player, i) => {
-        let selected = (i === 0);
+        const selected = (i === 0);
         return {
             display: player.get('name'),
             challenged: !!player.get('challenging'),
@@ -22,54 +22,56 @@ export default Ember.Component.extend({
     }).filter((player) => player.challenged);
   }),
 
-  incrementResults(player, position, framesWon, framesLost) {
-    player.set('position', position);
-    player.incrementProperty('games.wins', 1);
-    player.incrementProperty('frames.wins', framesWon);
-    player.incrementProperty('frames.losses', framesLost);
-    player.set('challenging', null);
+  _incrementResults(player, position, framesWon, framesLost) {
+    const isWinner = framesWon > framesLost;
+    const wins = isWinner ? player.get('games.wins') + 1 : player.get('games.wins');
+    const losses = isWinner ? player.get('games.losses') : player.get('games.losses') + 1;
+
+    player.setProperties({
+      position,
+      expiry: null,
+      challenging: null,
+      games: {
+        wins,
+        losses
+      },
+      frames: {
+        wins: player.get('frames.wins') + framesWon,
+        losses: player.get('frames.losses') + framesLost
+      }
+    });
   },
 
   _updateResults(players) {
-    let p1 = {name: this.get('nameOne'), score: Number(this.get('scoreOne'))};
-    let p2 = {name: this.get('nameTwo'), score: Number(this.get('scoreTwo'))};
+    const p1 = {name: this.get('nameOne'), score: Number(this.get('scoreOne'))};
+    const p2 = {name: this.get('nameTwo'), score: Number(this.get('scoreTwo'))};
 
     // Start with assuming nameOne is the winner
     let winner = players.findBy('name', p1.name);
     let loser = players.findBy('name', p2.name);
 
     if (p2.score > p1.score) {
-      let tmp = winner;
+      const tmp = winner;
       winner = loser;
       loser = tmp;
     }
 
-    let winnerScore = Math.max(p1.score, p2.score);
-    let loserScore = Math.min(p1.score, p2.score);
-    let wp = winner.get('position');
-    let lp = loser.get('position');
-    let topPosition = Math.min(wp, lp);
-    let lowerPosition = Math.max(wp, lp);
+    const winnerScore = Math.max(p1.score, p2.score);
+    const loserScore = Math.min(p1.score, p2.score);
+    const wp = winner.get('position');
+    const lp = loser.get('position');
+    const topPosition = Math.min(wp, lp);
+    const lowerPosition = Math.max(wp, lp);
 
     if (winnerScore > 3 || winnerScore < 0 || loserScore > 3 || loserScore < 0) {
-        throw new Error('Scores should be between 0 and 3');
+      throw new Error('Scores should be between 0 and 3');
     }
     if (winnerScore === loserScore) {
       throw new Error('There should be a winner');
     }
 
-    this.incrementResults(winner, topPosition, winnerScore, loserScore);
-    this.incrementResults(loser, lowerPosition, loserScore, winnerScore);
-
-    return players.save();
-  },
-
-  _removeExpiry(players) {
-    let player = players.findBy('name', this.get('nameOne'));
-    player.set('expiry', '');
-
-    player = players.findBy('name', this.get('nameTwo'));
-    player.set('expiry', '');
+    this._incrementResults(winner, topPosition, winnerScore, loserScore);
+    this._incrementResults(loser, lowerPosition, loserScore, winnerScore);
 
     return players.save();
   },
@@ -93,8 +95,8 @@ export default Ember.Component.extend({
     selectPlayer1(player) {
         this.set('nameOne', player);
         this.get('store').findAll('person').then((players) => {
-          let playerTwo = players.findBy('name', player);
-          let nameTwo = playerTwo ? playerTwo.get('challenging') : 'no challenger found';
+          const playerTwo = players.findBy('name', player);
+          const nameTwo = playerTwo ? playerTwo.get('challenging') : 'no challenger found';
           this.set('nameTwo', nameTwo);
         }).catch((error) => this.sendAction('setError', error));
     },
@@ -102,9 +104,7 @@ export default Ember.Component.extend({
     addGame() {
       this.get('store').findAll('person')
         .then(this._updateResults.bind(this))
-        .catch((error) => this.sendAction('setError', error))
-        .then(this._removeExpiry.bind(this))
-        .catch((error) => this.sendAction('setError', error))
+        .catch((error) => { throw new Error(error); })
         .then(this._createGame.bind(this))
         .catch((error) => this.sendAction('setError', error));
     }
